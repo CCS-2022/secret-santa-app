ff5538b9a28d6cf30126490b36d8db5ed421270a
CCS-2022 <128265192+CCS-2022@users.noreply.github.com>
2023-11-08 17:16:45 -0500

    Update UserController.java

--- src/main/java/com/ccs/secretsantaapp/controller/UserController.java
+++ src/main/java/com/ccs/secretsantaapp/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/secret-santa/user")
-@CrossOrigin(origins = "https://ss.cloudconsultingandsolutions.com/")
+@CrossOrigin(origins = "https://ssb.cloudconsultingandsolutions.com/")
 public class UserController {
     @Autowired
     private SecretSantaUserService userService;
