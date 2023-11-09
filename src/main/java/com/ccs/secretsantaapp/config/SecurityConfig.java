a737348f87ab461cac2a1355046e21051eb7f1fd
CCS-2022 <128265192+CCS-2022@users.noreply.github.com>
2023-11-08 20:20:58 -0500

    Update SecurityConfig.java

--- src/main/java/com/ccs/secretsantaapp/config/SecurityConfig.java
+++ src/main/java/com/ccs/secretsantaapp/config/SecurityConfig.java
@@ -36,9 +36,9 @@ public class SecurityConfig {
                 // CORS configuration
                 .cors().configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("*"));
+                   // config.setAllowedOrigins(Collections.singletonList("*"));
                     config.setAllowedMethods(Collections.singletonList("*"));
-                    config.setAllowCredentials(false);
+                    config.setAllowCredentials(true);
                     config.setAllowedHeaders(Collections.singletonList("*"));
                     config.setExposedHeaders(Arrays.asList("Authorization"));
                     config.setMaxAge(3600L);
